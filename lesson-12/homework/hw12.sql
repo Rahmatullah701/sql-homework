43ced5d7af214c092714bde79bf5234240cd72e5
Rahmatullah701 <olimjanov0809@gmail.com>
2025-08-03 19:16:50 +0500

    Create hw12.sql

---
+++ lesson-12/homework/hw12.sql
@@ -0,0 +1,91 @@
+--1
+SELECT 
+    p.firstName, 
+    p.lastName, 
+    a.city, 
+    a.state
+FROM Person p
+LEFT JOIN Address a 
+    ON p.personId = a.personId;
+
+Create table Person (personId int, firstName varchar(255), lastName varchar(255))
+Create table Address (addressId int, personId int, city varchar(255), state varchar(255))
+Truncate table Person
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen')
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
+Truncate table Address
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York')
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California')
+
+--2
+
+SELECT e.name AS Employee
+FROM Employee e
+JOIN Employee m
+  ON e.managerId = m.id
+WHERE e.salary > m.salary;
+
+Create table Employee (id int, name varchar(255), salary int, managerId int)
+Truncate table Employee
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL)
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL)
+
+--3
+SELECT email AS Email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
+
+
+--4
+SELECT *
+FROM Person p
+WHERE p.id = (
+    SELECT MIN(id)
+    FROM Person
+    WHERE email = p.email);
+
+--5
+
+SELECT DISTINCT g.ParentName
+FROM girls g
+WHERE g.ParentName NOT IN (
+    SELECT DISTINCT b.ParentName
+    FROM boys b);
+
+--6
+
+
+--7
+SELECT 
+    c1.Item AS [Item Cart 1],
+    c2.Item AS [Item Cart 2]
+FROM 
+    Cart1 c1
+FULL OUTER JOIN 
+    Cart2 c2
+ON 
+    c1.Item = c2.Item;
+
+
+--8
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+    ON c.id = o.customerId
+WHERE o.customerId IS NULL;
+
+--9
+SELECT 
+    s.student_id,
+    s.student_name,
+    subj.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects subj
+LEFT JOIN Examinations e
+    ON s.student_id = e.student_id AND subj.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, subj.subject_name
+ORDER BY s.student_id, subj.subject_name;
